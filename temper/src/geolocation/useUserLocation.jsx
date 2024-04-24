d1161259b5f724141db7e6cfe6ea4ac7bfec34ce
ShakerNasser <shaker.nasser@outlook.com>
2024-04-24 21:59:07 +0200

    postion- lat/long

--- temper/src/geolocation/useUserLocation.jsx
+++ temper/src/geolocation/useUserLocation.jsx
@@ -1,11 +1,9 @@
 // geolocation/useUserLocation.js
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 
 const useUserLocation = () => {
   const [position, setPosition] = useState(null);
   const [error, setError] = useState(null);
-  const [locationName, setLocationName] = useState(null); // Tillstånd för platsnamn
 
   useEffect(() => {
     const geo = navigator.geolocation;
@@ -15,26 +13,11 @@ const useUserLocation = () => {
     }
 
     const watchId = geo.watchPosition(
-      async (position) => {
+      (position) => {
         setPosition({
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
         });
-
-        // Omvänd geokodning för att hämta platsnamn från koordinater
-        try {
-          const response = await axios.get(
-            `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=AIzaSyCVT6efFTgELjYvAazwjclhWwGRJXIrhoE`
-          );
-
-          if (response.data.results.length > 0) {
-            const formattedAddress = response.data.results[0].formatted_address;
-            setLocationName(formattedAddress);
-          }
-        } catch (error) {
-          console.error('Error fetching location:', error);
-          setLocationName('Unknown location');
-        }
       },
       (error) => {
         setError(error.message);
@@ -44,7 +27,7 @@ const useUserLocation = () => {
     return () => geo.clearWatch(watchId);
   }, []);
 
-  return { position, error, locationName };
+  return { position, error };
 };
 
 export default useUserLocation;
